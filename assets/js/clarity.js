8252a73645c75236c501e7d43f9932a040d1bbf6
JepriAlber <jeprialber@gmail.com>
2021-09-10 16:10:54 +0700

    animasi untuk top bar

--- assets/js/clarity.js
+++ assets/js/clarity.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
-      
+    //----------sticky------------
+    var targetSticky = $('.site-header');
+    var positionScroll = 0;
+        $(window).on('scroll',function(){
+            var scroll = $(window).scrollTop();
+                if(scroll > 0){
+                    targetSticky.addClass('stiky-active');
+                    if(positionScroll > scroll){
+                        targetSticky.addClass('stiky-scroll');
+                    }else{
+                        targetSticky.removeClass('stiky-scroll');
+                    }
+
+                }else{
+                    targetSticky.removeClass('stiky-active');
+                    targetSticky.removeClass('stiky-scroll');
+                }
+            positionScroll =  scroll;  
+        });
+    //---------navbar humburger---------
     $(".btn-toggle-state").click(function () {
         $("#site-header").toggleClass("toggle-onactive");
     });
